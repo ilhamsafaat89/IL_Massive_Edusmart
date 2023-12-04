e7a40e980304544ea380eab3465136aba62afb51
estarossa666 <2010631170081@student.unsika.ac.id>
2023-12-04 16:36:00 +0700

    misc

---
+++ app/src/main/java/com/dasadarsa/edusmart/ortu/absensi/adapter/RecycleViewHomeWaliAbsensiAdapter.kt
@@ -0,0 +1,10 @@
+package com.dasadarsa.edusmart.ortu.absensi.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.dasadarsa.edusmart.ortu.absensi.AbsensiWaliFragment
+import com.dasadarsa.edusmart.ortu.absensi.data.AbsensiWaliData
+
+class RecycleViewHomeWaliAbsensiAdapter constructor(private val getActivity: AbsensiWaliFragment, private val AbsensiWaliList: List<AbsensiWaliData>) :
+RecyclerView.Adapter<RecycleViewHomeWaliAbsensiAdapter.MyViewHolder>(){
+    
+}
---
+++ app/src/main/java/com/dasadarsa/edusmart/ortu/absensi/data/AbsensiWaliData.kt
@@ -0,0 +1,3 @@
+package com.dasadarsa.edusmart.ortu.absensi.data
+
+data class AbsensiWaliData()
